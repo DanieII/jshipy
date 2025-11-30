ac25179cbb2a7c622decc9939b9f7220828c3c5b
Daniel Bogdanov <danielbogdanov1748@gmail.com>
2025-11-30 20:41:18 +0200

    Add requirements to django

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,4 +22,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    strictPort: true,
+    allowedHosts: [],
+  },
 })
